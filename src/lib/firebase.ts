1b3f4904a3c3268bb783d0d8c5c5e27a14bcc8a5
M B <txellbm@gmail.com>
2025-07-25 17:36:31 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,19 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  "projectId": "app-sometrik",
-  "appId": "1:352482910314:web:8c5b6b8c8d1b1c7c9b0e1a",
-  "storageBucket": "app-sometrik.appspot.com",
-  "apiKey": "AIzaSyA_S0m3Th1ngS3cr3t",
-  "authDomain": "app-sometrik.firebaseapp.com",
-  "messagingSenderId": "352482910314"
+  apiKey: "AIzaSyCxVPfW9aVFIYAaUu4Jr5Uix-YLIqHl1Xw",
+  authDomain: "app-sometrik.firebaseapp.com",
+  projectId: "app-sometrik",
+  storageBucket: "app-sometrik.firebasestorage.app",
+  messagingSenderId: "352482910314",
+  appId: "1:352482910314:web:2a9020e50fd6ffcbd4aa13",
+  measurementId: "G-D6DQHBNLF9"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
